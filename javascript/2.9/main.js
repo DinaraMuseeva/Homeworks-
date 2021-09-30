cbb2a1c531276cfffcb60bd3f1054a516b0ffe5d
DinaraMuseeva <museeva@bk.ru>
2021-09-30 16:44:18 +0300

    fifth-leddon-homework

---
+++ javascript/2.9/main.js
@@ -0,0 +1,7 @@
+5 > 4  // true
+"ананас" > "яблоко" //  false
+"2" > "12" //  true (сравниваем строки по символьно)
+undefined == null  // true
+undefined === null  // false
+null == "\n0\n"  // false  (null равен тлько undefined)
+null === +"\n0\n"  // false
---
+++ javascript/fifth-lesson-advanced/script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+let UserName = prompt("Как тебя зовут?", " ");
+let UserNickname = prompt("Твой никнейм?", " ");
+let UserAge = prompt("Сколько тебе лет?", " ");
+
+
+// alert(`Тебя зовут ${UserName}`);
+// alert(`Тебе ${UserAge} лет`);
+// alert(`Твой никнейм ${UserNickname}`);
+
+
+console.log(`Тебя зовут ${UserName}`);
+console.log(`Тебе ${UserAge} лет`);
+console.log(`Твой никнейм ${UserNickname}`);
