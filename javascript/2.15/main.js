ff0a1f4aad19086a044615d74e478972d5afedf1
DinaraMuseeva <museeva@bk.ru>
2021-10-09 12:17:55 +0300

    add_ seventh_lesson_homework

---
+++ javascript/2.15/main.js
@@ -0,0 +1,70 @@
+"use strict"
+
+/*
+Перепишите функцию, чтобы она делала то же самое, но без if, в одну строку.
+Сделайте два варианта функции checkAge:
+Используя оператор ?
+Используя оператор ||
+
+	function checkAge(age) {
+	if (age > 18) {
+		return true;
+	} else {
+		return confirm('Родители разрешили?');
+	}
+}
+
+*/
+
+// Используя оператор ?
+
+function checkAge(age) {
+	return (age > 18) ?  true :  confirm('Родители разрешили?');
+}
+let age = prompt("сколько лет");
+
+if (checkAge(age)) {
+	alert('Доступ получен');
+} else {
+	alert('Доступ закрыт');
+}
+
+//Используя оператор ||
+
+function checkAge(age) {
+	return age > 18 || confirm('Родители разрешили?');
+	
+}
+let age = prompt("сколько лет");
+
+if (checkAge(age)) {
+	alert('Доступ получен');
+} else {
+	alert('Доступ закрыт');
+}
+
+// Напишите функцию min(a, b), которая возвращает меньшее из чисел a и b.
+
+function getMin ( a,b) {
+	if (a<b){
+		return a;
+	} else{
+		return b
+	}
+}
+let result = getMin(2, 5);
+alert (result);
+
+// Напишите функцию pow(x, n), которая возвращает x в степени n.Иначе говоря, умножает x на себя n раз и возвращает результат.
+
+let x = prompt("x?");
+let n = prompt("n?");
+
+function pow (x,n) {
+	if (n > 0){
+		return x ** n;
+	} 
+}
+
+let result = pow(x,n);
+alert (result);
---
+++ javascript/2.17/main.js
@@ -0,0 +1,29 @@
+"use strict"
+/* ЗАДАНИЕ 
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Вы согласны?",
+  function() { alert("Вы согласились."); },
+  function() { alert("Вы отменили выполнение."); }
+);
+
+*/
+
+
+let ask = (question, yes, no) =>{ confirm(question)? yes() : no ()};
+
+
+// function ask(question, yes, no) {
+// 	if (confirm(question)) yes()
+// 	else no();
+// }
+
+ask(
+	"Вы согласны?",
+	 () =>{ alert("Вы согласились."); },
+	 () =>{ alert("Вы отменили выполнение."); }
+);
